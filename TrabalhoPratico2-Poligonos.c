c7e0c81ad30c7d242186818366c55dc729037bfd
unknown <leonardolouzadam@gmail.com>
2024-04-25 21:15:24 -0300

    alteracoes no codigo

--- TrabalhoPratico2-Poligonos.c
+++ TrabalhoPratico2-Poligonos.c
@@ -1,33 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct {
+typedef struct{
     float X;
     float Y;
-} Ponto;
+}Ponto;
 
 float AreaTriangulo(Ponto A, Ponto B, Ponto C);
 float AreaPoligono(Ponto vertices[], int n);
 
 int main(){
-    FILE *file;
+    FILE *pontos;
     int n;
 
-    file = fopen("Vertices.txt", "r");
-    if(file == NULL){
+    pontos = fopen("Vertices.txt", "r");
+    if(pontos == NULL){
         printf("Erro ao abrir o arquivo.\n");
         return 1;
     }
 
-    fscanf(file, "%d", &n);
+    fscanf(pontos, "%d", &n);
     Ponto vertices[n];
 
     int i;
     for(i = 0; i < n; i++){
-        fscanf(file, "%f %f", &vertices[i].X, &vertices[i].Y);
+        fscanf(pontos, "%f %f", &vertices[i].X, &vertices[i].Y);
     }
 
-    fclose(file);
+    fclose(pontos);
 
     float area = AreaPoligono(vertices, n);
     printf("A area do poligono eh: %.2f\n", area);
@@ -40,7 +40,7 @@ float AreaTriangulo(Ponto A, Ponto B, Ponto C){
 }
 
 float AreaPoligono(Ponto vertices[], int n){
-    float area = 0.0;
+    float area = 0;
     int i;
     for(i = 1; i < n - 1; i++) {
         area += AreaTriangulo(vertices[0], vertices[i], vertices[i + 1]);
